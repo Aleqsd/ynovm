150b1c5a6784b2254a1bbd6ba928f10aa71803fe
Thomas Alunni-Gradini <thomas.alunni-gradini@ynov.com>
2017-12-02 21:02:40 +0100

    escale & autonome dbs

---
+++ sql/autonome.sql
@@ -0,0 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  sam. 02 déc. 2017 à 19:56
+-- Version du serveur :  5.7.19
+-- Version de PHP :  5.6.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `autonome`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `station`
+--
+
+DROP TABLE IF EXISTS `station`;
+CREATE TABLE IF NOT EXISTS `station` (
+  `id` int(11) NOT NULL,
+  `x` int(11) NOT NULL,
+  `y` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `localisation` varchar(50) NOT NULL,
+  `temperature` double NOT NULL,
+  `hygrometrie` double NOT NULL,
+  `nebulosite` int(11) NOT NULL,
+  `anemometre` int(11) NOT NULL,
+  `pluviometrie` int(11) NOT NULL,
+  `etat` int(11) NOT NULL,
+  `etat_Pluvio` int(11) NOT NULL,
+  `etat_Anemo` int(11) NOT NULL,
+  `etat_Nebul` int(11) NOT NULL,
+  `etat_Hygro` int(11) NOT NULL,
+  `etat_Temp` int(11) NOT NULL,
+  `h_restart` int(11) DEFAULT NULL,
+  `remarques` varchar(200) NOT NULL,
+  `type` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `station`
+--
+
+INSERT INTO `station` (`id`, `x`, `y`, `nom`, `localisation`, `temperature`, `hygrometrie`, `nebulosite`, `anemometre`, `pluviometrie`, `etat`, `etat_Pluvio`, `etat_Anemo`, `etat_Nebul`, `etat_Hygro`, `etat_Temp`, `h_restart`, `remarques`, `type`) VALUES
+(1, 3, 6, 'autonome', 'Lyon', 16, 3, 5, 7, 3, 3, 2, 2, 2, 2, 2, NULL, '', 0);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
